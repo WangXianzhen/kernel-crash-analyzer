6239f85c1c83559e896d20f00c3fb79846ee0a66
David A. Desrosiers <setuid@gmail.com>
2019-01-30 17:45:20 -0500

    Refactoring: add support for passing in a directory, cleaner unpack

--- kca.py
+++ kca.py
@@ -13,9 +13,9 @@
 #
 # TODO: # to be checked off when completed
 #
-#  [ ] Add debug for incorrect kernel version numbers passed in
+#  [x] Add debug for incorrect kernel version numbers passed in
 #  [ ] Detect host environment, use dpkg vs. ar where necessary
-#  [ ] Add optional '-d' arg to specify an output directory, else pwd
+#  [x] Add optional '-d' arg to specify an output directory, else pwd
 #  [ ] Support cleanly slicing Call Trace pragma from logs
 #  [ ] Jinja2 support for output 'report' of final results
 #
@@ -45,21 +45,25 @@ class Kca:
 
 
 ###############################################################
-def download_kernel(app, kernel, headers):
+def download_kernel(app, dest_dir, kernel, kernel_size):
 
-    kernel_size = int(headers['Content-Length'])
+    kernel_path = '{}/{}'.format(dest_dir, kernel)
+
+    if not os.path.exists(dest_dir):
+        os.makedirs(dest_dir)
 
     do_download = False
-    if os.path.exists(kernel):
-        if len(open(kernel, 'rb').read()) != kernel_size:
+    if os.path.exists(kernel_path):
+        if len(open(kernel_path, 'rb').read()) != kernel_size:
             do_download = True
             output_file = open(kernel, "ab")
-            headers.update({'Range': 'bytes=%s-%s'
-                           % (os.path.getsize(kernel), kernel_size)})
+            app.headers.update({'Range': 'bytes=%s-%s'
+                               % (os.path.getsize(kernel_path),
+                                kernel_size)})
 
     else:
         do_download = True
-        output_file = open(kernel, "wb")
+        output_file = open(kernel_path, "wb")
 
     if do_download:
         print('Downloading...: {}{}'.format(app.k_url, kernel))
@@ -68,11 +72,11 @@ def download_kernel(app, kernel, headers):
                                    headers=app.headers, stream=True)
 
         bar_width = 20
-        progress = '#' * int(os.path.getsize(kernel) /
+        progress = '#' * int(os.path.getsize(kernel_path) /
                              (kernel_size / bar_width))
         bar_template = "%(label)s [{}%(bar)s] %(info)s".format(progress)
 
-        progress_width = bar_width - int(os.path.getsize(kernel) /
+        progress_width = bar_width - int(os.path.getsize(kernel_path) /
                                          (kernel_size / bar_width))
         with click.progressbar(kernel_ddeb.iter_content(1024),
                                bar_template=bar_template,
@@ -89,46 +93,47 @@ def download_kernel(app, kernel, headers):
 
 
 ###############################################################
-def unpack_debug_kernel(app, kver, kernel):
+def unpack_debug_kernel(app, dest_dir, kver, kernel):
+
+    kernel_path = './usr/lib/debug/boot/vmlinux-{}'.format(kver)
 
     print("Unpacking....: {}".format(kernel))
-    subprocess.run(['ar', 'x', kernel], check=True)
+    subprocess.run(['ar', 'x', '{}/{}'.format(dest_dir, kernel),
+                    '{}/data.tar.xz'.format(dest_dir)], check=True)
 
-    if not os.path.exists('{}_debug'.format(kver)):
-        os.makedirs('{}_debug'.format(kver))
-
-    print("Extracting...: kernel 'boot/vmlinux-{}'...".format(kver))
+    print("Extracting...: '{}'...".format(kernel_path))
     subprocess.run(['tar', 'xvf', 'data.tar.xz', '-C',
-                   '{}_debug'.format(kver), kernel_path], check=True)
+                   dest_dir, kernel_path],
+                   check=True)
 
-    print("Removing.....: control.tar.gz, data.tar.gz")
+    print("Removing.....: data.tar.gz")
     os.unlink('data.tar.xz')
 
 
 ###############################################################
-def dump_symbols(app, kver):
+def dump_symbols(app, dest_dir, kver):
 
     with open("debug.S", "wb", 0) as out:
-        subprocess.run(['objdump', '-l', '-D',
-                        '{}_debug/usr/lib/debug/boot/vmlinux-{}'.format(kver, kver)],
+        subprocess.run(['objdump', '-l', '-d',
+                        '{}/usr/lib/debug/boot/vmlinux-{}'
+                        .format(dest_dir, kver)],
                        stdout=out, check=True)
 
 
 @click.command()
 @click.option('-a', '--arch', 'arch',
-              type=click.Choice(['amd64',
-                                 'i386',
-                                 'armhf',
-                                 'arm64',
-                                 's390x',
-                                 'ppc64']),
+              type=click.Choice(['amd64', 'i386',
+                                 'armhf', 'arm64',
+                                 's390x', 'ppc64']),
               required=True,
               help='Architecture to filter on')
 @click.option('-k', '--kernel-version', 'kver',
               required=True,
               help='Kernel version to filter on')
+@click.option('-d', '--dest-dir', 'dest_dir',
+              default=os.getcwd())
 ###############################################################
-def main(arch, kver):
+def main(arch, kver, dest_dir):
 
     app = Kca()
 
@@ -141,9 +146,14 @@ def main(arch, kver):
     for kernel in links:
         if arch in kernel and kver in kernel:
             r = app.session.head('{}/{}'.format(app.k_url, kernel))
-            download_kernel(app, kernel, r.headers)
-            unpack_debug_kernel(app, kver, kernel)
-            dump_symbols(app, kver)
+            kernel_size = int(r.headers['Content-Length'])
+            download_kernel(app, dest_dir, kernel, kernel_size)
+            unpack_debug_kernel(app, dest_dir, kver, kernel)
+            dump_symbols(app, dest_dir, kver)
+        else:
+            print("Kernel {} not found. Is this the version kernel?"
+                  .format(kver))
+            return
 
 
 if __name__ == "__main__":
